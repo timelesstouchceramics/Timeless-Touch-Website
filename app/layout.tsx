db1612d021c4223db78201444dd5a1c8b892e1b7
Faez <faez@xmaagency.com>
2026-01-06 16:04:40 +0400

    Adding gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,12 @@ import "./globals.css";
 import { Providers } from "@/components/providers";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Metadata } from "next";
+import Script from "next/script";
 
 import { Geist, Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+
+const GTM_ID = "GTM-M2XN8HTP";
 
 const fontPrimary = Geist({
   subsets: ["latin"],
@@ -32,9 +35,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`antialiased ${fontPrimary.variable} ${fontSecondary.variable}`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <NavigationServer />
         <Providers>{children}</Providers>
         <Footer />
